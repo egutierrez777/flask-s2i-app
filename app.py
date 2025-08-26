fc1db672b974c4bf5a8862e2673054ef87de2fc6
Student User <student@workstation.lab.example.com>
2025-08-26 10:09:20 -0400

    First modify in the main app

--- app.py
+++ app.py
@@ -5,5 +5,9 @@ app = Flask(__name__)
 def hello():
     return "¡Hola desde Flask en OpenShift con S2I!"
 
+@app.route('/hello/<name>')
+def greet(name):
+    return f"Hola {name}, ¡Bienvenido a OpenShift!"
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8080)
